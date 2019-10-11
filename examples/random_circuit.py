720944a682f915ea8a98e9d9f9c5c0a4287dfda5
Misha Aghamalyan <misha.aghamalyan@gmail.com>
2019-10-11 15:31:46 +0400

    [FIX] Run over many random circuits for each program depth and take mean fidelity for better results.

--- examples/random_circuit.py
+++ examples/random_circuit.py
@@ -47,10 +47,14 @@ def run_with_qc(qc, lengths):
     data = []
 
     for i in lengths:
-        p = get_random_circuit(qc.qubits(), i)
-        res = qc.run_and_measure(p, 1000)
-        fidelity = 1 - np.mean(res[0])
-        data.append(fidelity)
+        fidelities = []
+        for j in range(20):
+            p = get_random_circuit(qc.qubits(), i)
+            res = qc.run_and_measure(p, 10000)
+            fidelity = 1 - np.mean(res[0] * res[1])
+            fidelities.append(fidelity)
+        mean_fidelity = np.mean(np.array(fidelities))
+        data.append(mean_fidelity)
 
     return data
 
